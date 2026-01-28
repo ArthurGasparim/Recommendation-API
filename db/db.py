3b3e6957827053882d8d3ed8c3364adb23d344e4
ArthurGasparim <arthurggasparim@gmail.com>
2026-01-28 18:24:50 -0300

    Creating the Database on Postgres

--- db/db.py
+++ db/db.py
@@ -1,7 +1,29 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-class Book(SQLModel,table=True):
+class Books(SQLModel,table=True):
     id:int | None = Field(default=None,primary_key=True)
-    name: str = Field(index=True)
+    book_id: int = Field(index=True)
+    original_title: str = Field(index=True)
+    authors:str = Field(index=True)
+    average_rating:float = Field(index=True)
+
+class books_tags(SQLModel,table=True):
+    goodreads_book_id:int = Field(primary_key=True)
+    tag_id:int = Field(primary_key=True)
+    count:int=Field(index=True)
+
+class ratings(SQLModel,table=True):
+    book_id:int =  Field(primary_key=True)
+    user_id:int = Field(primary_key=True)
+    rating:int = Field(index=True)
+
+class tags(SQLModel,table=True):
+    tag_id:int = Field(primary_key=True)
+    tag_name:str = Field(index=True)
+
+class to_read(SQLModel,table=True):
+    book_id:int =  Field(primary_key=True)
+    user_id:int = Field(primary_key=True)
+    
 
 Postgres_db_name = "bookstore_db"
 host = "localhost"
